407fb0d7c57868d5b6eeda535ea9f7368f5a67e1
wondmaru <wondmaru2@gmail.com>
2018-12-21 11:16:49 -0600

    This is My other Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Classwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
